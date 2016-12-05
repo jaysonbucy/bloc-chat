5f2795430db13f90cff3114498f8cd80ade85b8d
jaysonbucy <jaysonbucy@legalshieldcorp.com>
2016-12-05 13:57:48 -0600

    wip

---
+++ app/scripts/controllers/AddRoomCtrl.js
@@ -0,0 +1,25 @@
+(function(){
+  function AddRoomCtrl($uibModal){
+    this.name = '';
+    AddRoomCtrl.open = function(){
+      var modalOpen = $uibModal.open({
+        templateUrl: 'new_room.html',
+        controller: function($scope, $uibModalInstance){
+          $scope.cancel = function () {
+            $uibModalInstance.dismiss('cancel');
+          };
+          $scope.submit = function(){
+            $uibModalInstance.close($scope.name);
+          };
+        }
+      });
+    };
+    modalInstance.result.then(function (name) {
+      var newRoom = firebase.database().ref().child("rooms");
+      newRoom.push({"name":name});
+    });
+  }
+  angular
+    .module('blocChat')
+    .factory('$uibModal', 'AddRoomCtrl', [AddRoomCtrl]);
+})();
