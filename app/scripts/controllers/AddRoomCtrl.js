8dd7afcf7911646625594653cdc690654e848418
jaysonbucy <jaysonbucy@legalshieldcorp.com>
2016-12-06 09:27:29 -0600

    wip

--- app/scripts/controllers/AddRoomCtrl.js
+++ app/scripts/controllers/AddRoomCtrl.js
@@ -1,25 +1,14 @@
 (function(){
-  function AddRoomCtrl($uibModal){
-    this.name = '';
-    this.open = function(){
-      var modalOpen = $uibModal.open({
-        templateUrl: 'new_room.html',
-        controller: function($scope, $uibModalInstance){
-          $scope.cancel = function () {
-            $uibModalInstance.dismiss('cancel');
-          };
-          $scope.submit = function(){
-            $uibModalInstance.close($scope.name);
-          };
-        }
-      });
+  function AddRoomCtrl($scope, $uibModal){
+    $scope.cancel = function () {
+      $uibModalInstance.dismiss('cancel');
     };
-    modalInstance.result.then(function (name) {
-      var newRoom = firebase.database().ref().child("rooms");
-      newRoom.push({"name":name});
-    });
+    $scope.submit = function(){
+      $uibModalInstance.close($scope.name);
+    };
   }
+
   angular
     .module('blocChat')
-    .factory('AddRoomCtrl', ['$uibModal', AddRoomCtrl]);
+    .controller('AddRoomCtrl', ['$scope','$uibModal', AddRoomCtrl])
 })();
--- app/scripts/controllers/MainCtrl.js
+++ app/scripts/controllers/MainCtrl.js
@@ -1,12 +1,21 @@
 (function(){
-  function MainCtrl(Room, $scope){
+  function MainCtrl(Room, $uibModal, $scope){
     this.mainTitle = "/assets/images/bloc-logo-white.png";
     this.roomArray = Room;
     $scope.rooms = this.roomArray.all;
+
+    this.openModal = function(){
+      var modal = $uibModal.open({
+        templateUrl: '/templates/new_room.html',
+        controller: 'AddRoomCtrl as addRoom'
+      });
+      modal.result.then(function (name) {
+        firebase.database().ref().child("rooms").push({"name":name});
+      });
+    };
   }
 
   angular
     .module('blocChat')
-    .controller('MainCtrl', ['Room', '$scope', MainCtrl]);
+    .controller('MainCtrl', ['Room', '$uibModal', '$scope', MainCtrl]);
 })();
-;
