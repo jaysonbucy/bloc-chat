fa5bc08eadba2803c6e5d21747a9c5dca8869e3e
jaysonbucy <jaysonbucy@legalshieldcorp.com>
2016-12-06 17:11:49 -0600

    As a user, I want to create chat rooms

--- app/scripts/controllers/AddRoomCtrl.js
+++ app/scripts/controllers/AddRoomCtrl.js
@@ -1,9 +1,24 @@
 (function(){
-  function AddRoomCtrl(){
-
+  function AddRoomCtrl($uibModal){
+    this.openModal = function(){
+      var modal = $uibModal.open({
+        templateUrl: '/templates/new_room.html',
+        controller: function($scope, $uibModalInstance){
+          $scope.cancel = function(){
+            $uibModalInstance.dismiss('cancel');
+          };
+          $scope.submit = function(){
+            $uibModalInstance.close($scope.name);
+          };
+        }
+      });
+      modal.result.then(function (name){
+        firebase.database().ref().child("rooms").push({"name": name});
+      });
+    };
   }
 
   angular
     .module('blocChat')
-    .controller('AddRoomCtrl', AddRoomCtrl)
+    .controller('AddRoomCtrl', ['$uibModal', AddRoomCtrl])
 })();
--- app/scripts/controllers/MainCtrl.js
+++ app/scripts/controllers/MainCtrl.js
@@ -1,28 +1,11 @@
 (function(){
-  function MainCtrl(Room, $uibModal, $scope){
+  function MainCtrl(Room, $scope){
     this.mainTitle = "/assets/images/bloc-logo-white.png";
     this.roomArray = Room;
     $scope.rooms = this.roomArray.all;
-
-    this.openModal = function(){
-      var modal = $uibModal.open({
-        templateUrl: '/templates/new_room.html',
-        controller: function($scope, $uibModalInstance){
-          $scope.cancel = function(){
-            $uibModalInstance.dismiss('cancel');
-          };
-          $scope.submit = function(){
-            $uibModalInstance.close($scope.name);
-          };
-        }
-      });
-      modal.result.then(function (name){
-        firebase.database().ref().child("rooms").push({"name": name});
-      });
-    };
   }
 
   angular
     .module('blocChat')
-    .controller('MainCtrl', ['Room', '$uibModal', '$scope', MainCtrl]);
+    .controller('MainCtrl', ['Room', '$scope', MainCtrl]);
 })();
