758d3fc8c6ab6aa76c618de3a3f5d2f22eaf3541
jaysonbucy <jaysonbucy@legalshieldcorp.com>
2016-12-12 14:02:00 -0600

    wip

--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -1,15 +1,26 @@
 (function(){
-  function Message($firebaseArray){
-    function getMessagesByRoomId(roomId){
-          var messages = firebase.database().ref().child("messages").orderByChild("roomId");
-          return $firebaseArray(messages.equalTo(roomId));
-        }
+  function Message($firebaseArray, $cookies){
     return{
-      getMessagesByRoomId: getMessagesByRoomId
+      getMessagesByRoomId: function(roomId){
+        var messages = firebase.database().ref().child("messages").orderByChild("roomId");
+        return $firebaseArray(messages.equalTo(roomId));
+      }
+      send: function(roomId, message){
+        var currentUser = $cookies.get('blocChatCurrentUser');
+        var time = new Date();
+        time = time.getHours() + ':' + time.getMinutes();
+        var newMessage = {
+          content: message,
+          roomId: roomId,
+          sentAt: time,
+          username: currentUser
+        };
+        messages.$add(newMessage);
+      }
     };
   }
 
   angular
     .module('blocChat')
-    .factory('Message', ['$firebaseArray', Message])
+    .factory('Message', ['$firebaseArray', '$cookies', Message])
 })();
